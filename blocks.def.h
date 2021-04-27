bc71eb23d7b6da6bf03d4098c1057c8f94d65dda
Nikan <nikankad@protonmail.com>
2021-04-27 22:37:25 +0800

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
+	// make sure to put the full path to the script for it to work
+	// most important thing is to make sure the scripts are executable in order for them to work
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	// {"", "/home/user/.config/dwmblocks/scripts/network.sh",	2,		0},
 	{"", "/home/user/.config/dwmblocks/scripts/song.sh",1,		0},
-
 	{"", "/home/user/.config/dwmblocks/scripts/bitcoin.py",	180,		0},
 	{"", "/home/user/.config/dwmblocks/scripts/memory.sh",	10,		0},
 	{"", "/home/user/.config/dwmblocks/scripts/weather.py",	180,		0},
